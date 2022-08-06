0f141e50577cbeb571203c2e6db8a9d5d1a14b4e
nuseehr <nuseehr@naver.com>
2022-08-06 23:32:06 +0900

    baekjoon: 11659 구간합구하기4 풀이 수정

--- src/main/java/com/onlineJudge/baekjoon/implementation/SumOfIntervals/P11659.java
+++ src/main/java/com/onlineJudge/baekjoon/implementation/SumOfIntervals/P11659.java
@@ -13,12 +13,14 @@ import java.util.StringTokenizer;
 public class P11659 {
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        // 문자열을 입력 받는다. 공백을 기준으로 문자열 split한다.
         StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
 
         int N = Integer.parseInt(stringTokenizer.nextToken());
         int M = Integer.parseInt(stringTokenizer.nextToken());
-
         long[] S = new long[N+1];
+
+        // 문자열을 입력 받는다. 공백을 기준으로 문자열 split한다.
         stringTokenizer = new StringTokenizer(bufferedReader.readLine());
 
         for (int i = 1; i <= N; i++) {
@@ -26,6 +28,7 @@ public class P11659 {
         }
 
         for (int i = 0; i < M; i++) {
+            // 문자열을 입력 받는다. 공백을 기준으로 문자열 split한다.
             stringTokenizer = new StringTokenizer(bufferedReader.readLine());
             int j = Integer.parseInt(stringTokenizer.nextToken());
             int k = Integer.parseInt(stringTokenizer.nextToken());
